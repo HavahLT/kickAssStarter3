dfc73eafead78671093991363f07cbb421cfc237
claireeee <simplonco@debian>
2017-03-18 22:31:10 +0100

    ajout home connectee

--- src/pages/Home-connected/Home-connected.js
+++ src/pages/Home-connected/Home-connected.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import '../../Reset.css';
+import '../../App.css';
 import Headerconnecte from '../../composants/Headerconnecte/Headerconnecte.js';
+import Bandeau from '../../composants/Bandeau/Bandeau.js';
+import Projetsaleatoires from '../../composants/Projetsaleatoires/Projetsaleatoires.js';
+import Partenaires from '../../composants/Partenaires/Partenaires.js';
+import Footer from '../../composants/Footer/Footer.js';
+import '../../Data.js';
 
 class Homeconnected extends Component {
   render() {
     return (
-      <div className="Homeconnected">
-
+      <div className="App">
+      <div className="container-principal">
         <Headerconnecte />
+        <Bandeau />
+        <div className='container'>
+          <Projetsaleatoires />
+        </div>
+          <Partenaires />
+        </div>
 
-
+        <Footer />
       </div>
     );
   }
 }
+
 
 export default Homeconnected;
